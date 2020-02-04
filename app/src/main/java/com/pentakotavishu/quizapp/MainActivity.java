ec6efa5f3a44d23da166784e5fbac4f156a2cce2
vishpentak <vishpentak@gmail.com>
2020-02-04 09:58:19 -0500

    lab done

--- app/src/main/java/com/pentakotavishu/quizapp/MainActivity.java
+++ app/src/main/java/com/pentakotavishu/quizapp/MainActivity.java
@@ -3,12 +3,44 @@ package com.pentakotavishu.quizapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    Button submitButton;
+    EditText responseText;
+    TextView displayText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        submitButton=findViewById(R.id.clickButton);
+        responseText=findViewById(R.id.responseEditText);//what the user types
+        displayText=findViewById(R.id.textBox);//the text on top of what the user types
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("Hello mom");
+                Log.i("testButton", "Hi dad! "+responseText.getText().toString());
+                displayText.setText(displayText.getText()+" "+responseText.getText());//changes the "Hello TJ" to "Hello TJ" + whatever the user typed
+            }
+        });
+        responseText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if(!hasFocus) {
+                    if(responseText.getText().toString().equals("TJ")){
+                        displayText.setText("TJ Rocks!");
+                        responseText.setText("");
+                        responseText.setHint("That's a good name.");
+                    }
+                }
+            }
+        });
+
     }
 }
